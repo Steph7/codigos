cd66f54f218f70c65b4f32ee7fcd110087dd25b0
Steph7 <Steph7@users.noreply.github.com>
2021-01-23 14:10:15 -0300

    enviar codigo

--- PDC/Aula Prática 7/problema5.py
+++ PDC/Aula Prática 7/problema5.py
@@ -1,2 +1,19 @@
 # _*_coding: utf-8 _*_
 
+def definirMedias(aluno):
+    dadosAlunos = aluno.split(" ")
+    nome = dadosAlunos[0]
+    notas = dadosAlunos[1:]
+    soma = 0
+    for x in notas:
+        soma = soma + float(x)
+    media = soma / len(notas)
+    if(media >= 60):
+        print("Nome: %s - Média: %.2f" % (nome, media))
+
+f = open("notas.txt", "r")
+texto = f.read()
+alunos = texto.split("\n")
+
+for dados in alunos:
+    definirMedias(dados)
