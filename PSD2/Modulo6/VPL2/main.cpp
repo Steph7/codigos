1839a788af733eea3b2fbfb00f5cb065be170291
Steph7 <Steph7@users.noreply.github.com>
2021-03-23 17:00:19 -0300

    .

--- PSD2/Modulo6/VPL2/main.cpp
+++ PSD2/Modulo6/VPL2/main.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
+#include <string.h>
+
+struct IndiceNaoinicializado {
+  int index;
+};
+
+struct IndiceNegativo {
+  int index;
+};
+
+struct IndiceMaiorArranjo {
+  int index;
+}; 
+
 
 template <class T, int N> class BoundedArray {
   public:
     void set(int index, T value) {
-      buf[index] = value;
+      if(index < 0) throw IndiceNegativo{index};
+      if(index >= N) throw IndiceMaiorArranjo{index};
+      else{
+        buf[index] = value;
+        testador[index] = 1;
+      }
     }
     T get(int index) {
+      if(index < 0) throw IndiceNegativo{index};
+      if(index >= N) throw IndiceMaiorArranjo{index};
+      if(testador[index] != 1) throw IndiceNaoinicializado{index};
       return buf[index];
     }
   private:
     T buf[N];
+    int testador[N];
 };
 
 template <class T> void testArray() {
@@ -26,7 +49,17 @@ template <class T> void testArray() {
       } else if (action == 'g') {
         std::cout << a.get(index) << std::endl;
       }
-    } catch (...) {
+    }
+    catch (IndiceNaoinicializado e){
+      std::cerr << "Erro: indice nao inicializado." << std::endl;
+    }
+    catch (IndiceNegativo e){
+      std::cerr << "Erro: indice negativo." << std::endl;
+    }
+    catch (IndiceMaiorArranjo e){
+      std::cerr << "Erro: indice maior que arranjo." << std::endl;
+    }
+    catch (...) {
       std::cerr << "Erro desconhecido." << std::endl;
     }
   }
